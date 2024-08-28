3cb465e28fb12f72a4f07b629ab3c8fe67166fbf
Antonio <antonio.moreira@ugba.br>
2024-08-28 17:57:47 -0300

    lex

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,13 +1,23 @@
-/* token.h */
-
 typedef enum {
-        ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
-        CHAR,
-	STR,
-	ERROR
-} token_t; 
+    ID = 256,   // Identificadores
+    NUM,        // Números
+    IF,         // Palavra reservada "if"
+    ELSE,       // Palavra reservada "else"
+    WHILE,      // Palavra reservada "while"
+    RETURN,     // Palavra reservada "return"
+    LTE,        // Operador <=
+    GTE,        // Operador >=
+    EQ,         // Operador ==
+    NEQ,        // Operador !=
+    ASSIGN,     // Operador =
+    PLUS,       // Operador +
+    MINUS,      // Operador -
+    MULT,       // Operador *
+    DIV,        // Operador /
+    LT,         // Operador <
+    GT,         // Operador >
+    CHAR,       // Caractere
+    STR,        // String
+    ERROR       // Erro léxico
+} token_t;
 
